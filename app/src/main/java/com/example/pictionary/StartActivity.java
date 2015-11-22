03388f787cb93f46da3019ce31a5ed6c9c0cdfd4
LucianC <lucian.cucu@gmail.com>
2015-11-22 14:24:11 +0100

    Base Prototype including Join and Create Activities

--- app/src/main/java/com/example/pictionary/StartActivity.java
+++ app/src/main/java/com/example/pictionary/StartActivity.java
@@ -1,18 +1,27 @@
 package com.example.pictionary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
 
-    //test GIT test GIT test GIT
+    //creating new branch
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // INIT CREATE BUTTON
+        setButtonCreateListener();
+
+        // INIT JOIN BUTTON
+        setButtonJoinListener();
     }
 
     @Override
@@ -35,5 +44,25 @@ public class StartActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setButtonCreateListener(){
+        Button buttonCreate = (Button) findViewById(R.id.button_create);
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void setButtonJoinListener(){
+        Button buttonJoin = (Button) findViewById(R.id.button_join);
+        buttonJoin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
